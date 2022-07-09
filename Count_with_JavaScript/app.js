f9a1694df0f87022ff04bed144b5cf1af242e805
Prakash Khatua <103178764+PrakashSDE@users.noreply.github.com>
2022-07-10 00:06:59 +0530

    Update app.js

--- Count_with_JavaScript/app.js
+++ Count_with_JavaScript/app.js
@@ -1,6 +1,6 @@
-
 let count =0;
-  const value= document.querySelector("#value");
+  
+const value= document.querySelector("#value");
   const decrease= document.querySelector(".btn_decrease");
   const increase= document.querySelector(".btn_increase");
   const reset= document.querySelector(".btn_reset");
