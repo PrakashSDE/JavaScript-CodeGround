f4ec2fe90dd7446e65c24e2a4b54b382aa10abc4
Prakash Khatua <103178764+PrakashSDE@users.noreply.github.com>
2022-07-10 00:04:15 +0530

    Add files via upload

---
+++ Count_with_JavaScript/app.js
@@ -0,0 +1,24 @@
+
+let count =0;
+  const value= document.querySelector("#value");
+  const decrease= document.querySelector(".btn_decrease");
+  const increase= document.querySelector(".btn_increase");
+  const reset= document.querySelector(".btn_reset");
+
+  decrease.addEventListener("click" , function(){
+    count--;
+    value.textContent = count;
+  });
+
+  increase.addEventListener("click" , function(){
+    count++;
+    value.textContent = count;
+  });
+
+  reset.addEventListener("click" , function(){
+    count=0;
+    value.textContent = count;
+  });
+
+
+
