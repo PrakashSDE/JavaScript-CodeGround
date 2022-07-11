23d28485c0fe8e858a513bd9e455134744227c38
Prakash Khatua <103178764+PrakashSDE@users.noreply.github.com>
2022-07-11 20:02:05 +0530

    Update app.js

--- Background_Changer/app.js
+++ Background_Changer/app.js
@@ -1,8 +1,6 @@
-
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
-
 
 btn.addEventListener("click", function () {
   var randomColor = Math.floor(Math.random()*16777215).toString(16);
