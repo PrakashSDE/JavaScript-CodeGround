e0d166097a18ac670fbfa767b99d8b01c4ae9ae6
Prakash Khatua <103178764+PrakashSDE@users.noreply.github.com>
2023-04-22 15:25:05 +0530

    Add files via upload

---
+++ JavaScript Inside/object.js
@@ -0,0 +1,9 @@
+const car = {
+          brand: 'Ford',
+          model: 'Fiesta',
+          start: function() {
+          console.log(`Start with brand ${this.brand}`)
+          }
+         }
+         
+ 
